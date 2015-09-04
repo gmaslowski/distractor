80e8e68ccada6c3890f3423860ebce48e495f49a
gmaslowski <gregmaslowski@gmail.com>
2015-09-04 18:57:44 +0200

    #8: Fixing imports.

--- src/main/scala/com/fp/distractor/core/transport/TransportRegistry.scala
+++ src/main/scala/com/fp/distractor/core/transport/TransportRegistry.scala
@@ -2,7 +2,8 @@ package com.fp.distractor.core.transport
 
 import akka.actor.{Actor, ActorLogging, Props}
 import com.fp.distractor.core.transport.TransportRegistry.SendBroadcast
-import com.fp.distractor.core.transport.api.TransportApi.{Message, Say}
+import com.fp.distractor.core.transport.api.Message
+import com.fp.distractor.core.transport.api.TransportApi.Say
 import com.fp.distractor.registry.ActorRegistry
 
 object TransportRegistry {
@@ -14,7 +15,7 @@ object TransportRegistry {
 
 class TransportRegistry extends Actor with ActorLogging with ActorRegistry {
 
-  override def receive = handleRegistry orElse(handle)
+  override def receive = handleRegistry orElse (handle)
 
   def handle: Receive = {
     case SendBroadcast(broadcast) =>
