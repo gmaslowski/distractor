e9e9414d6adf42b0bdda271eb6e30ca5cba1436b
gmaslowski <gregmaslowski@gmail.com>
2016-01-19 20:35:49 +0100

    #13: Merging develop.

--- src/main/scala/com/gmaslowski/distractor/transport/skype/SkypeTransport.scala
+++ src/main/scala/com/gmaslowski/distractor/transport/skype/SkypeTransport.scala
@@ -1,8 +1,7 @@
 package com.gmaslowski.distractor.transport.skype
 
-import akka.actor.{Props, Actor}
-import com.fp.distractor.registry.ActorRegistry.RegisterMsg
-import com.skype.{ApplicationAdapter, Skype, Stream}
+import akka.actor.{Actor, Props}
+import com.gmaslowski.distractor.registry.ActorRegistry.RegisterMsg
 
 object SkypeTransport {
   def props = Props[SkypeTransport]
@@ -14,25 +13,25 @@ class SkypeTransport extends Actor {
     context.actorSelection("akka://distractor/user/distractor/transport-registry") ! RegisterMsg("skype", self)
 
 
-//    var application = Skype.addApplication("skype-transport")
-//
-//    application.addApplicationListener(new ApplicationAdapter() {
-//
-//      override def connected(stream: Stream) = {
-//        printApplicationAndStreamName("connected:", stream)
-//      }
-//
-//      override def disconnected(stream: Stream) = {
-//        printApplicationAndStreamName("disconnected:", stream)
-//      }
-//
-//      def printApplicationAndStreamName(header: String, stream: Stream) = {
-//        println(header + stream.getApplication().getName() + "-" + stream.getId())
-//      }
-//    })
-//
-//    application.connectToAll()
-//    Skype.chat()
+    //    var application = Skype.addApplication("skype-transport")
+    //
+    //    application.addApplicationListener(new ApplicationAdapter() {
+    //
+    //      override def connected(stream: Stream) = {
+    //        printApplicationAndStreamName("connected:", stream)
+    //      }
+    //
+    //      override def disconnected(stream: Stream) = {
+    //        printApplicationAndStreamName("disconnected:", stream)
+    //      }
+    //
+    //      def printApplicationAndStreamName(header: String, stream: Stream) = {
+    //        println(header + stream.getApplication().getName() + "-" + stream.getId())
+    //      }
+    //    })
+    //
+    //    application.connectToAll()
+    //    Skype.chat()
   }
 
   override def receive: Receive = {
