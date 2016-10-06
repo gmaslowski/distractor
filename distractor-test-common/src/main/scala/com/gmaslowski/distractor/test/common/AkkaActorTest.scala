b05ee4d6c3c2ce512c9e6f87c9247ede02ca3d83
gmaslowski <gregmaslowski@gmail.com>
2016-10-06 14:05:59 +0200

    Specifying empty config for testing.

--- distractor-test-common/src/main/scala/com/gmaslowski/distractor/test/common/AkkaActorTest.scala
+++ distractor-test-common/src/main/scala/com/gmaslowski/distractor/test/common/AkkaActorTest.scala
@@ -2,6 +2,7 @@ package com.gmaslowski.distractor.test.common
 
 import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit}
+import com.typesafe.config.ConfigFactory
 import org.scalatest._
 
-abstract class AkkaActorTest extends TestKit(ActorSystem("actor-test-system")) with Matchers with WordSpecLike with ImplicitSender
+abstract class AkkaActorTest extends TestKit(ActorSystem("actor-test-system", ConfigFactory.empty())) with Matchers with WordSpecLike with ImplicitSender
